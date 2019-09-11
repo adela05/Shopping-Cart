1d81770aa3a120d8686b4ae6f5912e1cbb4370ea
Adelita Esparza <aesparza051@gmail.com>
2019-09-10 22:58:33 -0500

    started on frontend files

--- src/main/frontend-shopping/src/app/app.module.ts
+++ src/main/frontend-shopping/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, NavbarComponent ],
+	imports: [ BrowserModule, AppRoutingModule, HttpClientModule, FormsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
