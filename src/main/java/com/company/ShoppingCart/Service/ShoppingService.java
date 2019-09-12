06538120cbb86236f7677665587d0417a5142f95
Adelita Esparza <aesparza051@gmail.com>
2019-09-12 10:58:46 -0500

    updates on Service layer

--- src/main/java/com/company/ShoppingCart/Service/ShoppingService.java
+++ src/main/java/com/company/ShoppingCart/Service/ShoppingService.java
@@ -35,25 +35,29 @@ public class ShoppingService {
         return null;
     }
 
-    // Price multiply by qty
-//    public void priceByQty(Float price, Integer quantity){
-//        price = price * quantity;
-//    }
-    // Sales Tax 10 percent multiply by price on Non-Exempt Items
-//    public void salesTax(List<Shopping> exempt){
-//        //float price = 0; don't need to add this
-//        float total = 0;
-//        double tax = 0;
-//        for(int i = 0; i < exempt.size(); i++){
-//            //get the price (i) for each item
-//            if(!exempt.equals("Books") || !exempt.equals("Food") || !exempt.equals("Medical Supplies")){
-//                tax = .10 * price / 100;
-//                tax = Math.round(tax * 20.0)/20.0;
-//            } else{
-//                total += tax;
-//            }
-//        }
+    public void purchase(Shopping[] shopping){
 
-  //  }
+    }
+
+     // Price multiply by qty
+    public Float priceByQty(Float price, Integer quantity){
+        float total = price * quantity;
+    }
+    // Sales Tax 10 percent multiply by price on Non-Exempt Items
+    public void salesTax(List<Shopping> exempt){
+        float total = 0;
+        double tax = 0;
+
+        for(int i = 0; i < exempt.size(); i++){
+            total=this.priceByQty(exempt.get(i).getPrice(), exempt.get(i).getQuantity());
+            if(!exempt.equals("Books") || !exempt.equals("Food") || !exempt.equals("Medical Supplies")){
+                tax = .10 * total;
+                tax = Math.round(tax * 20.0)/20.0;
+            } else{
+                total += tax;
+            }
+        }
+
+  }
 
 }
